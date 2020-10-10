3964e1806d94595b3b6c8a82a13e37f491cff470
Jakub Jabłoński <jjablonski.it@gmail.com>
2020-10-10 15:19:07 +0200

    added express template and test route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("test");
+import express from "express";
+import { PORT } from "./config";
+
+(async () => {
+  const app = express();
+
+  // Test path
+  app.get("/", (_req, res) => {
+    return res.send("OK");
+  });
+
+  app.listen(PORT, () =>
+    console.log(`server running on http://localhost:${PORT}`)
+  );
+})();
